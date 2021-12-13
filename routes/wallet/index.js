3c4a4f0102c5ea5bc75e37621d7d6b9dd2fdc462
Koartifact <kor7and3@korea.ac.kr>
2021-12-13 22:23:56 +0900

    mnemonic wallet project

---
+++ routes/wallet/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const lightwallet = require("eth-lightwallet");
+const fs = require('fs');
+
+// TODO : lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
+router.post('/newMnemonic', async(req,res) => {
+  let mnemonic;
+
+  try {
+      mnemonic = lightwallet.keystore.generateRandomSeed();
+      res.json({mnemonic});
+  } catch(err) {
+      console.log(err);
+  }
+});
+
+
+// TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
+router.post('/newWallet', async(req, res) => {
+  let password = req.body.password
+  let mnemonic = req.body.mnemonic;
+
+  try {
+      lightwallet.keystore.createVault({
+              password: password, 
+              seedPhrase: mnemonic,
+              hdPathString: "m/0'/0'/0'"
+          },
+          function (err, ks) {
+              ks.keyFromPassword(password, function (err, pwDerivedKey) {
+                  ks.generateNewAddress(pwDerivedKey, 1);
+
+                  let address = (ks.getAddresses()).toString();
+                  let keystore = ks.serialize();
+
+                  fs.writeFile('wallet.json',keystore,function(err,data){
+                      if(err) {
+                          res.json({code:999,message:"실패"});
+                      } else {
+                          res.json({code:1,message:"성공"});
+                      }
+                  });
+              });
+          }
+      );
+  } catch (exception) { 
+    console.log("NewWallet ==>>>> " + exception);
+  }
+});
+
+module.exports = router;
